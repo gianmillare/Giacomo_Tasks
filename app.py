1532350785933680f20eaf743ccd82979bfbd627
gianmillare <gianmillaregsm@gmail.com>
2020-12-29 18:38:15 -0800

    completed the register function

--- app.py
+++ app.py
@@ -13,7 +13,30 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+conn = sqlite3.connect('giacomo.db', check_same_thread=False)
+c = conn.cursor()
 
+app.route("/register", method=["GET", "POST"])
+def register():
+    """ Register a new user """
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        if request.form.get("name") == "" or request.form.get("username") == "" or request.form.get("password") == "":
+            return render_template("errorpage.html")
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return render_template("errorpage.html")
+        else:
+            name = request.form.get("name")
+            username = request.form.get("username")
+            password = generate_password_hash(request.form.get("password"))
+            try: 
+                c.execute("INSERT INTO housemates (name, username, password) VALUES (?, ?, ?)", (name, username, password))
+                conn.commit()
+            except:
+                return render_template("errorpage.html")
+            
+            return render_template("register_success.html")
 
 @app.route("/")
 def index():
